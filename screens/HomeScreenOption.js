6e7022c787a5523001d998924d5e52b7266d62af
kanchan7c <kanchan.chaudhary77@gmail.com>
2023-01-25 15:59:26 +0530

    basic project settings

---
+++ screens/HomeScreenOption.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+
+export class HomeScreenOption extends Component {
+  render() {
+    return (
+      <View>
+        <Text>HomeScreenOption</Text>
+      </View>
+    );
+  }
+}
+
+export default HomeScreenOption;
