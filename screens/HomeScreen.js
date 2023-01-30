eb571a17b82a7faf540fd8857c1874a0fbf9588e
kanchan7c <kanchan.chaudhary77@gmail.com>
2023-01-30 10:12:47 +0530

    started with header, avatar text and icons added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { Image, SafeAreaView, Text, View } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text className='text-purple-500'>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      {/* header  */}
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image
+          source={require('../assets/app_images/avatar.jpg')}
+          className='h-7 w-7 bg-yellow-300 p-4 rounded-full'
+        />
+        <View>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <FontAwesome name='chevron-down' size={24} />
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
